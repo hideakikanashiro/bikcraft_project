6f187c3ac3190779db5ec95fe43095d7e8def5db
Eduardo Kanashiro <141886603+hideakikanashiro@users.noreply.github.com>
2024-08-01 16:40:24 -0300

    script_url_bic

--- js/script.js
+++ js/script.js
@@ -5,9 +5,30 @@ function ativarLink(link){
 const url = location.href;
 const href = link.href;
 
+// includes ele compara a url com o href. Se no href tiver os mesmos caracteres que url, ele executa o doido.
 if (url.includes (href)){
 link.classList.add ("ativo");
 }
 }
 
 links.forEach(ativarLink);
+
+
+
+
+
+
+// Pegar os valores da URL da pagina orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+const elemento = document.getElementById(parametro);
+
+// Um verificador para ver se o elemento realmente existe. Se o elemento existe, ele marca a caixinha de check
+if (elemento) {
+elemento.checked = true;
+}
+}
+
+parametros.forEach (ativarProduto);
