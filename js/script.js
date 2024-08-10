b688149ea12bd39bae6c0ca969e86a6e1ac73ca9
hideakikanashiro <hideaki.eduardo@gmail.com>
2024-08-09 21:56:41 -0300

    modelos_bicicletas

--- js/script.js
+++ js/script.js
@@ -55,3 +55,25 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+
+
+// BICICLETAS IMAGENS CARROSSEL
+const galeria = document.querySelectorAll('.bicicleta-img img');
+const galeriaContainer = document.querySelector('.bicicleta-img');
+
+function trocarImagem(event) {
+   const img = event.currentTarget;
+   const media = matchMedia('(min-width: 1000px)').matches;
+   if (media) {
+      galeriaContainer.prepend(img);
+      console.log (galeriaContainer)
+   }
+}
+
+function eventosGaleria(img) {
+   img.addEventListener('click', trocarImagem);
+}
+
+
+galeria.forEach(eventosGaleria)
